abc4bd733d12d2856d123596b2d4a6b525f5af64
Brian Tiemann <btman@lionking.org>
2017-05-28 21:56:01 -0400

    Use ModelSerializer for ProductSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 
+from ims.models import Product
+
 
 class ClientSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     company_name = serializers.CharField(max_length=150)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    item_number = serializers.CharField(max_length=12)
-    packing = serializers.IntegerField()
-    cases_inventory = serializers.IntegerField()
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'item_number', 'packing', 'cases_inventory']

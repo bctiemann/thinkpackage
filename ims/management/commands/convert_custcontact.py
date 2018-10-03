e0ac893e08eba611bf994ca4f9b1b591b8cf6e77
Root@scar <root@scar.lionwire.com>
2018-10-02 21:58:49 -0400

    More fields for convert_custcontact script

--- ims/management/commands/convert_custcontact.py
+++ ims/management/commands/convert_custcontact.py
@@ -25,28 +25,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         key = options.get('key', None)
+        if not key:
+            print('Missing --key <key>')
+            raise SystemExit
 
         aes = AESCipher(key)
 
         for custcontact in CustContact.objects.all():
-            print custcontact
             password = self.remove_control_chars(aes.decrypt(custcontact.password))
-            print password
 
             try:
                 user = User.objects.get(email=custcontact.email)
             except User.DoesNotExist:
-                print('Creating {0}'.format(custcontact.email))
+                print('Creating {0} ({1})'.format(custcontact.email, password))
                 user = User.objects.create_user(
                     email = custcontact.email,
                     password = password,
                 )
-                user.first_name = custcontact.first_name
-                user.last_name = custcontact.last_name
-                user.phone_number = custcontact.phone_number
-                user.notes = custcontact.notes
-
-                user.save()
+            user.first_name = custcontact.first_name
+            user.last_name = custcontact.last_name
+            user.phone_number = custcontact.phone_number
+            user.notes = custcontact.notes
+            user.last_login = custcontact.last_login
+            user.is_active = custcontact.is_active
+            user.save()
 
             client_user, created = ClientUser.objects.get_or_create(
                 user = user,

2f5a3bc376c01c5d4280e3997cd34b11035bbaff
Brian Tiemann <btman@mac.com>
2019-05-04 10:09:39 -0400

    Sync prod/dev settings

--- thinkpackage/settings_dev.py
+++ thinkpackage/settings_dev.py
@@ -44,7 +44,12 @@ MEDIA_URL = '/media/'
 LOG_DIR = '/usr/local/www/thinkpackage-dj/logs'
 LOGGING['handlers']['logfile']['filename'] = LOG_DIR + '/django.log'
 
+#TWO_FACTOR_SMS_GATEWAY = 'two_factor.gateways.twilio.gateway.Twilio'
 TWO_FACTOR_SMS_GATEWAY = 'two_factor.gateways.fake.Fake'
+TWO_FACTOR_CALL_GATEWAY = 'two_factor.gateways.twilio.gateway.Twilio'
+TWILIO_ACCOUNT_SID = 'AC1e9dd3090c8ce40529da714a5e93c935'
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_CALLER_ID = '404-800-7627'
 
 #EMAIL_HOST = 'mail.lionking.org'
 EMAIL_HOST = 'localhost'

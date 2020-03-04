d1096216521a9ef9d1166706dd9059d9de1d28c1
Brian Tiemann <btman@mac.com>
2020-03-04 09:54:12 -0500

    Validate current password

--- ims/forms.py
+++ ims/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from django.utils.safestring import mark_safe
 from django.contrib.auth.forms import SetPasswordForm, PasswordResetForm, AuthenticationForm
+from django.contrib.auth import (
+    login, authenticate, get_user_model, password_validation, update_session_auth_hash,
+)
 from django.http import HttpResponse, JsonResponse, Http404, HttpResponseForbidden
+from django.utils.translation import gettext, gettext_lazy as _
 
 from localflavor.us.forms import USStateField, USZipCodeField
 from localflavor.us.us_states import STATE_CHOICES
@@ -70,13 +74,27 @@ class UserLoginForm(AuthenticationForm):
 
 
 class PasswordChangeForm(SetPasswordForm):
-    existing_password = forms.CharField(max_length=50)
+    current_password = forms.CharField(
+        label=_("Current password"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
 
     def __init__(self, *args, **kwargs):
-        # request = kwargs.pop("request")
         self.instance = kwargs.pop('instance')
-        print(kwargs)
         super().__init__(self.instance, *args, **kwargs)
         self.fields['new_password2'].label = 'Repeat password'
+        self.error_messages['incorrect_current_password'] = 'Current password is incorrect.'
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+    def clean_current_password(self):
+        current_password = self.cleaned_data.get('current_password')
+        current_password_correct = authenticate(username=self.instance.email, password=current_password)
+        if not current_password_correct:
+            raise forms.ValidationError(
+                self.error_messages['incorrect_current_password'],
+                code='incorrect_current_password',
+            )
+        return current_password

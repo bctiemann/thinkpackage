3054c023565f579baaa7356e21ee9c7a27e21f70
Root@scar <root@scar.lionwire.com>
2020-03-04 21:28:13 -0500

    Don't import six

--- ims/management/commands/import.py
+++ ims/management/commands/import.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.utils import six, timezone
+from django.utils import timezone
 
 import os
 import unicodedata, re

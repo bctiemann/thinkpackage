5e1f4a0c2b4f573976a42d8324efd180830a5360
Brian Tiemann <btman@mac.com>
2020-03-08 19:10:25 -0400

    Update SERVER_HOST for prod

--- thinkpackage/settings_prod.py
+++ thinkpackage/settings_prod.py
@@ -6,7 +6,7 @@ except ImportError:
 
 DEBUG = False
 
-SERVER_HOST = 'thinkpackage.grotto11.com'
+SERVER_HOST = 'secure.thinkpackage.com'
 SERVER_URL_PREFIX = 'http://'
 SERVER_BASE_URL = '{0}{1}'.format(SERVER_URL_PREFIX, SERVER_HOST)
 

a7e39fb794e9daca34394484117049d7c73dcc96
Root@scar <root@scar.lionwire.com>
2020-03-08 16:12:42 -0400

    Workaround for Safari email click-tracking on password reset

--- thinkpackage/settings_common.py
+++ thinkpackage/settings_common.py
@@ -289,3 +289,7 @@ CLIENTACCESS_EMAIL = 'clientaccess@thinkpackage.com'
 INFINITE_SCROLL_PAGE_SIZE = 30
 COUNTRIES_FIRST = ['US', 'CA']
 # COUNTRIES_FIRST_BREAK = '(Select country)'
+
+# Workaround Safari email click-tracking issue for password reset links
+CSRF_COOKIE_SAMESITE = None
+SESSION_COOKIE_SAMESITE = None

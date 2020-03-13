7ed894f4793ee3263e8405a1dbff2f3afb63a97b
Brian Tiemann <btman@mac.com>
2020-03-13 09:15:02 -0400

    Initial test

--- ims/tests.py
+++ ims/tests.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-
-
 from django.test import TestCase
 
-# Create your tests here.
+from ims.models import Client, User, Shipment, Transaction, Receivable
+
+class ShipmentTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client.objects.create(company_name="Client 1")
+
+    def test_shipment_created_as_pending(self):
+        shipment = Shipment.objects.create(client=self.client)
+        self.assertEqual(shipment.is_pending, True)

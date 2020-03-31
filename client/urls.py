af79d375c5536250ff9c2d6579552bdec6dbd168
Brian Tiemann <btman@mac.com>
2020-03-31 18:59:01 -0400

    Convert clients urls to paths

--- client/urls.py
+++ client/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path, register_converter
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
@@ -10,33 +11,33 @@ from client import views as client_views
 
 
 urlpatterns = [
-    url(
-        r'^login/$',
+    path(
+        'login/',
         client_views.LoginView.as_view(),
         name='login',
     ),
-    url(r'^$', client_views.home, name='home'),
-    url(r'^sign_out/', ims_views.LogoutView.as_view(next_page='client:login'), name='sign-out'),
+    path('', client_views.home, name='home'),
+    path('sign_out/', ims_views.LogoutView.as_view(next_page='client:login'), name='sign-out'),
 
-    url(r'^change_password/$', client_views.change_password, name='change-password'),
-    url(r'^dismiss_password_prompt/$', client_views.dismiss_password_prompt, name='dismiss-password-prompt'),
+    path('change_password/', client_views.change_password, name='change-password'),
+    path('dismiss_password_prompt/', client_views.dismiss_password_prompt, name='dismiss-password-prompt'),
 
-    url(r'^profile/$', client_views.profile, name='profile'),
-    url(r'^profile/locations/$', client_views.profile_locations, name='profile-locations'),
-    url(r'^profile/location/(?P<location_id>\d+)/$', client_views.profile_location_detail, name='profile-location-detail'),
+    path('profile/', client_views.profile, name='profile'),
+    path('profile/locations/', client_views.profile_locations, name='profile-locations'),
+    path('profile/location/<int:location_id>/', client_views.profile_location_detail, name='profile-location-detail'),
 
-    url(r'^inventory/$', client_views.inventory, name='inventory'),
-    url(r'^inventory/list/$', client_views.inventory_list, name='inventory-list'),
-    url(r'^inventory/delivery/(?P<shipment_id>\d+)/products/$', client_views.delivery_products, name='delivery-products'),
-    url(r'^inventory/request_delivery/$', client_views.inventory_request_delivery, name='inventory-request_delivery'),
+    path('inventory/', client_views.inventory, name='inventory'),
+    path('inventory/list/', client_views.inventory_list, name='inventory-list'),
+    path('inventory/delivery/<int:shipment_id>/products/', client_views.delivery_products, name='delivery-products'),
+    path('inventory/request_delivery/', client_views.inventory_request_delivery, name='inventory-request_delivery'),
 
-    url(r'^history/$', client_views.history, name='history'),
+    path('history/', client_views.history, name='history'),
 
-    url(r'^reorder/$', client_views.reorder, name='reorder'),
+    path('reorder/', client_views.reorder, name='reorder'),
 
-    url(r'^select/(?P<client_id>\d+)/$', client_views.select_client, name='select'),
+    path('select/<int:client_id>/', client_views.select_client, name='select'),
 
-    url(r'^product/(?P<product_id>\d+)/history/$', client_views.product_history, name='product-history'),
-    url(r'^product/(?P<product_id>\d+)/report/$', client_views.product_report, name='product-report'),
-    url(r'^shipment/(?P<shipment_id>\d+)/docs/$', client_views.shipment_docs, name='shipment-docs'),
+    path('product/<int:product_id>/history/', client_views.product_history, name='product-history'),
+    path('product/<int:product_id>/report/', client_views.product_report, name='product-report'),
+    path('shipment/<int:shipment_id>/docs/', client_views.shipment_docs, name='shipment-docs'),
 ]

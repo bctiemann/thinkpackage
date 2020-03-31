37fcd461906e60f32f7ace091ab661d4a5e55aa6
Brian Tiemann <btman@mac.com>
2020-03-31 19:15:26 -0400

    Move api urls to paths

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.urls import path, register_converter
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
@@ -10,10 +11,10 @@ from api import views as api_views
 
 
 urlpatterns = [
-    url(r'^clients/$', api_views.GetClients.as_view(), name='clients'),
-    url(r'^(?P<client_id>\d+)/products/$', api_views.GetClientProducts.as_view(), name='client-products'),
-    url(r'^async_task/(?P<task_id>[0-9a-f-]+)/status/$', api_views.AsyncTaskStatus.as_view(), name='async-task-status'),
+    path('clients/', api_views.GetClients.as_view(), name='clients'),
+    path('<int:client_id>/products/', api_views.GetClientProducts.as_view(), name='client-products'),
+    path('async_task/<uuid:task_id>/status/', api_views.AsyncTaskStatus.as_view(), name='async-task-status'),
 
-    url(r'^users-ac/(?P<term>.+)/$', api_views.AutocompleteUsers.as_view(), name='autocomplete-users'),
-    url(r'^user/(?P<user_id>\d+)/$', api_views.UserAPIView.as_view(), name='user'),
+    path('users-ac/<term>/', api_views.AutocompleteUsers.as_view(), name='autocomplete-users'),
+    path('user/<int:user_id>/', api_views.UserAPIView.as_view(), name='user'),
 ]

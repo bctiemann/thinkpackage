a0a8b9f1ceda1377259af6f4511d41d734aa49c0
Brian Tiemann <btman@mac.com>
2020-04-09 18:40:32 -0400

    Add refresh_token and auth_check methods

--- ims/sps.py
+++ ims/sps.py
@@ -4,6 +4,9 @@ from redis.client import Redis
 
 from django.conf import settings
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 REDIS_KEY = 'sps_api_token'
 
@@ -12,6 +15,7 @@ class SPSService(object):
 
     TOKEN_URL = 'https://auth.spscommerce.com/oauth/token'
     TRANSACTION_URL = 'https://api.spscommerce.com/transactions/v2/'
+    AUTH_CHECK_URL = 'https://api.spscommerce.com/auth-check'
 
     def __init__(self, *args, **kwargs):
         self.redis_client = Redis(connection_pool=BlockingConnectionPool())
@@ -23,6 +27,10 @@ class SPSService(object):
         return {
             'Authorization': f'Bearer {self.token}'
         }
+
+    def refresh_token(self):
+        self.redis_client.delete(REDIS_KEY)
+        self.token = self.get_token()
 
     def get_token(self):
         payload = {
@@ -36,6 +44,11 @@ class SPSService(object):
         result = response.json()
         self.redis_client.set(REDIS_KEY, value=result.get('access_token'), ex=result.get('expires_in'))
         return result.get('access_token')
+
+    def auth_check(self):
+        response = requests.get(self.AUTH_CHECK_URL, headers=self.get_headers())
+        response.raise_for_status()
+        logger.info('Auth successful.')
 
     def create_transaction(self, binary_data):
         transaction_url = f'{self.TRANSACTION_URL}'

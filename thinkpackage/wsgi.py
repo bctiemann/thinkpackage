9156d522d290fdd6db4823cc34b993bd628a37d7
Brian Tiemann <btman@mac.com>
2020-04-20 19:33:51 -0400

    Remove settings_dev.py and unneeded env var passing

--- thinkpackage/wsgi.py
+++ thinkpackage/wsgi.py
@@ -10,13 +10,6 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os, sys
 from django.core.wsgi import get_wsgi_application
 
-env_variables_to_pass = [
-    'SECRET_KEY',
-    'DJANGO_SETTINGS_MODULE',
-]
 
 def application(environ, start_response):
-    # pass the WSGI environment variables on through to os.environ
-    for var in env_variables_to_pass:
-        os.environ[var] = environ.get(var, '')
     return get_wsgi_application()(environ, start_response)

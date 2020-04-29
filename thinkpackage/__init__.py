af53dbfb1084c8f739171a8baa7bd33e7b1602a7
Brian Tiemann <btman@mac.com>
2020-04-29 15:23:59 -0400

    Properly init celery app as per docs

--- thinkpackage/__init__.py
+++ thinkpackage/__init__.py
@@ -1,3 +1,9 @@
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+
+__all__ = ['celery_app']
+
 import pymysql
 
 pymysql.version_info = (1, 4, 6, 'final', 0)  # change mysqlclient version

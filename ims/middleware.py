d84f6ceb7beb3e0ff76056927698b4af63b242c1
Brian Tiemann <btman@mac.com>
2020-05-13 19:11:21 -0400

    Fix log string

--- ims/middleware.py
+++ ims/middleware.py
@@ -25,7 +25,7 @@ class LoginRequiredMiddleware(MiddlewareMixin):
         if not request.user.is_authenticated:
             resolved = resolve(request.path_info)
             current_route_name = resolved.url_name
-            logger.info(current_route_name, resolved.app_name)
+            logger.info(f'{current_route_name} {resolved.app_name}')
 
             if not resolved.app_name or resolved.app_name == 'admin':
                 return None

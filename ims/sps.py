7dc4a0dc9c28c6e00f7b4245895f61931c1e8a95
Brian Tiemann <btman@mac.com>
2020-07-04 14:44:06 -0400

    Build out SPS API client with all endpoints

--- ims/sps.py
+++ ims/sps.py
@@ -16,6 +16,7 @@ class SPSService(object):
     TOKEN_URL = 'https://auth.spscommerce.com/oauth/token'
     TRANSACTION_URL = 'https://api.spscommerce.com/transactions/v2/'
     AUTH_CHECK_URL = 'https://api.spscommerce.com/auth-check'
+    PROCESSING_REPORTS_URL = 'https://api.spscommerce.com/transactions/v4/file-processing-reports'
 
     def __init__(self, *args, **kwargs):
         self.redis_client = Redis(connection_pool=BlockingConnectionPool())
@@ -23,18 +24,18 @@ class SPSService(object):
         if saved_token:
             self.token = saved_token.decode()
         else:
-            self.token = self.get_token()
+            self.token = self._get_token()
 
-    def get_headers(self):
+    def _get_headers(self):
         return {
             'Authorization': f'Bearer {self.token}'
         }
 
-    def refresh_token(self):
+    def _refresh_token(self):
         self.redis_client.delete(REDIS_KEY)
-        self.token = self.get_token()
+        self.token = self._get_token()
 
-    def get_token(self):
+    def _get_token(self):
         payload = {
             "grant_type": "client_credentials",
             "client_id": settings.SPS_APP_ID,
@@ -48,13 +49,59 @@ class SPSService(object):
         return result.get('access_token')
 
     def auth_check(self):
-        response = requests.get(self.AUTH_CHECK_URL, headers=self.get_headers())
+        response = requests.get(self.AUTH_CHECK_URL, headers=self._get_headers())
         response.raise_for_status()
         logger.info('Auth successful.')
 
-    def create_transaction(self, binary_data):
+    def get_transaction_url(self, file_path='', file_key=''):
         transaction_url = f'{self.TRANSACTION_URL}'
-        response = requests.post(transaction_url, data=binary_data, headers=self.get_headers())
-        # response.raise_for_status()
-        result = response.json()
-        print(result)
+        if file_path:
+            transaction_url += f'{file_path}/'
+        transaction_url += file_key
+        return transaction_url
+
+    def create_transaction(self, binary_data, file_path='', file_key=''):
+        transaction_url = self.get_transaction_url(file_path, file_key)
+        response = requests.post(transaction_url, data=binary_data, headers=self._get_headers())
+        response.raise_for_status()
+        return response.json()
+
+    def get_transaction(self, file_path='', file_key=''):
+        transaction_url = self.get_transaction_url(file_path, file_key)
+        response = requests.get(transaction_url, headers=self._get_headers())
+        response.raise_for_status()
+        return response.content
+
+    def list_transactions(self, file_path='', limit=None):
+        transaction_url = self.get_transaction_url(file_path)
+        transaction_url += '*'
+        if limit:
+            transaction_url += f'?limit={limit}'
+        response = requests.get(transaction_url, headers=self._get_headers())
+        response.raise_for_status()
+        return response.json()
+
+    def delete_transaction(self, file_path='', file_key=''):
+        transaction_url = self.get_transaction_url(file_path, file_key)
+        response = requests.delete(transaction_url, headers=self._get_headers())
+        response.raise_for_status()
+        return response.content
+
+    def procesing_reports(self, page_number=None, page_size=None, start_date_time=None, end_date_time=None):
+        '''
+        :param page_number: int
+        :param page_size: int
+        :param start_date_time: datetime in ISO format (2016-06-23T09:07:21), i.e. from datetime.isoformat()
+        :param end_date_time: datetime in ISO format (2016-06-23T09:07:21), i.e. from datetime.isoformat()
+        :return: json
+        '''
+        url = self.PROCESSING_REPORTS_URL
+        params = {
+            'pageNumber': page_number,
+            'pageSize': page_size,
+            'start_date_time': start_date_time,
+            'end_date_time': end_date_time,
+        }
+        response = requests.get(url, params=params, headers=self._get_headers())
+        response.raise_for_status()
+        return response.json()

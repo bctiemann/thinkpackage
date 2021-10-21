a88b04d4015abf024eb0114ba63d532e0176d73f
Brian Tiemann <btman@mac.com>
2021-10-21 15:53:02 -0400

    Log IP address using HTTP_X_FORWARDED_FOR in login

--- ims/views.py
+++ ims/views.py
@@ -258,7 +258,7 @@ class LoginView(LoginView):
     @method_decorator(ensure_csrf_cookie)
     def dispatch(self, request, *args, **kwargs):
         logger.info(f'{request.resolver_match.app_name} login: {request.user} {request.method} '
-                    f'{request.POST.get("auth-username")} {request.META.get("REMOTE_ADDR")} '
+                    f'{request.POST.get("auth-username")} {request.META.get("HTTP_X_FORWARDED_FOR")} '
                     f'{request.POST.get("csrfmiddlewaretoken")}')
         if settings.LOG_AUTH:
             auth_logger.info(f'{request.user} {request.POST}')

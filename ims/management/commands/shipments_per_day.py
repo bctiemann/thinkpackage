bee5176cdc48b2da1c0feee9b603452572fc0c37
Brian Tiemann <btman@mac.com>
2024-05-08 14:38:26 -0400

    Include today in shipments_per_day

--- ims/management/commands/shipments_per_day.py
+++ ims/management/commands/shipments_per_day.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 
     @staticmethod
     def daterange(start_date, end_date):
-        for n in range(int((end_date - start_date).days)):
+        for n in range(int((end_date - start_date).days) + 1):
             yield start_date + timedelta(n)
 
     def handle(self, *args, **options):

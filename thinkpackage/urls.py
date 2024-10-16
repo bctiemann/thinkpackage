a2721ff52f1979d56175331fc7bc7ef14059da15
Brian Tiemann <btman@mac.com>
2024-10-15 20:53:38 -0400

    Remove url import

--- thinkpackage/urls.py
+++ thinkpackage/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.urls import path, register_converter
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
